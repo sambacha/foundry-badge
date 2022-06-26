39ae795d4f8207b37c25a563255abf86528f0b0f
sam bacha <sam@freighttrust.com>
2022-06-26 15:06:56 -0700

    root

---
+++ index.ts
@@ -0,0 +1,17 @@
+export const DESCRIPTION = "<placeholder text>";
+
+export const STYLES = {
+  BADGE_TYPE: "shield",
+  SOCIAL: "social",
+};
+
+export const DEFAULT_BADGE = {
+    BADGE_TYPE: "shield",
+    SOCIAL: "social",
+  };
+
+export enum COLOR {
+  Default = "blue",
+  Green = "2ea44f",
+  LogoDefault = "white",
+}
